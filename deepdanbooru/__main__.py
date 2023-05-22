9cef4a78088e479a9118e02c1eac8748f05aa45c
Kamuri Amorim <myk.gata14@gmail.com>
2023-05-22 14:30:22 -0300

    feat: rebase

--- deepdanbooru/__main__.py
+++ deepdanbooru/__main__.py
@@ -7,7 +7,7 @@ import os
 import deepdanbooru as dd
 import tensorflow.lite as tflite
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 
 @click.version_option(prog_name="DeepDanbooru", version=__version__)
