ee5be3fb8a728b629486f28649f15db1006fb40a
Kamuri Amorim <27641366+kamuridesu@users.noreply.github.com>
2024-07-25 11:11:47 -0300

    chore: bump version

--- deepdanbooru/__main__.py
+++ deepdanbooru/__main__.py
@@ -7,7 +7,7 @@ import os
 import deepdanbooru as dd
 import tensorflow.lite as tflite
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 
 @click.version_option(prog_name="DeepDanbooru", version=__version__)
