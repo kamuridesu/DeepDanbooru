49725695c29d30e4ceb6814c902a2c473fb51947
Kamuri Amorim <myk.gata14@gmail.com>
2025-04-24 18:15:00 -0300

    ci: bump version

--- deepdanbooru/__main__.py
+++ deepdanbooru/__main__.py
@@ -1,13 +1,14 @@
+import os
 import sys
-from typing_extensions import Required
+import warnings
 
 import click
-import warnings
-import os
-import deepdanbooru as dd
 import tensorflow.lite as tflite
+from typing_extensions import Required
 
-__version__ = "1.0.3"
+import deepdanbooru as dd
+
+__version__ = "1.0.4"
 
 
 @click.version_option(prog_name="DeepDanbooru", version=__version__)
@@ -185,7 +186,7 @@ def grad_cam(project_path, target_path, output_path, threshold):
 )
 @click.option("--verbose", default=False, is_flag=True)
 def evaluate(
-    target_paths, # I guess its this one
+    target_paths,  # I guess its this one
     project_path,
     model_path,
     tags_path,
@@ -199,7 +200,7 @@ def evaluate(
 ):
     if verbose:
         warnings.filterwarnings("always")
-        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
+        os.environ["TF_CPP_MIN_LOG_LEVEL"] = "0"
     dd.commands.evaluate(
         target_paths,
         project_path,
@@ -213,6 +214,7 @@ def evaluate(
         folder_filters,
         verbose,
     )
+
 
 @main.command("conv2tflite", help="Convert saved model into tflite model.")
 @click.option(
@@ -231,16 +233,27 @@ def evaluate(
 @click.option("--optimize-default", default=True, is_flag=True)
 @click.option("--optimize-experimental-sparsity", default=False, is_flag=True)
 @click.option("--verbose", default=False, is_flag=True)
-def conv2tflite(project_path, model_path, save_path, optimize_default, optimize_experimental_sparsity, verbose):
+def conv2tflite(
+    project_path,
+    model_path,
+    save_path,
+    optimize_default,
+    optimize_experimental_sparsity,
+    verbose,
+):
     if verbose:
         warnings.filterwarnings("always")
-        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
+        os.environ["TF_CPP_MIN_LOG_LEVEL"] = "0"
     if not optimize_default and not optimize_experimental_sparsity:
         raise Exception("optimization method must be specified")
     op = []
-    if optimize_default: op = [tflite.Optimize.DEFAULT]
-    if optimize_experimental_sparsity: op.append(tflite.Optimize.EXPERIMENTAL_SPARSITY)
-    dd.commands.convert_to_tflite_from_from_saved_model(project_path, model_path, save_path, op, verbose=verbose)
+    if optimize_default:
+        op = [tflite.Optimize.DEFAULT]
+    if optimize_experimental_sparsity:
+        op.append(tflite.Optimize.EXPERIMENTAL_SPARSITY)
+    dd.commands.convert_to_tflite_from_from_saved_model(
+        project_path, model_path, save_path, op, verbose=verbose
+    )
 
 
 if __name__ == "__main__":
